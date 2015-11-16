65b6202e9c600c36cf75ff3ff2b44c379ba4923c
marco amaral <amaral.marco@gmail.com>
2015-11-16 16:55:27 +0000

    iz work

--- MyServer/public/app/controllers/izcontrollers.js
+++ MyServer/public/app/controllers/izcontrollers.js
@@ -1,27 +1,81 @@
 var izcontrollers = angular.module('izcontrollers',[]);
 
-izcontrollers.controller('homeCtrl', ['$scope', '$location', function($scope, $location){
-	
-}]);
-
-izcontrollers.controller('loginCtrl', ['$scope', '$rootScope', '$location', '$http', '$window', function($scope, $rootScope, $location, $http, $window) {
-	$scope.user = {};
+izcontrollers.controller('loginCtrl', ['$scope', '$rootScope', '$location', '$http', '$window', 'alertService', function($scope, $rootScope, $location, $http, $window, alertService) {
+	if ($window.localStorage.token){
+		$location.path('/opendoor');
+	}
+	$rootScope.closeAlert = alertService.closeAlert;
+	$scope.addAlert = function (type,msg){
+		alertService.add(type,msg);
+	}
+	$scope.credentials = {};
 	$scope.auth_error = '';
 	$scope.authenticate = function(){
-		console.log($scope.user);
-		$http.post('/authenticate', $scope.user)
-			.success(function (data, status, headers, config) {
-				console.log('login success');
-				$window.sessionStorage.token = data.token;
-				$rootScope.isAuthenticated = true;
-				$('#loginModal').modal('hide');
-				$location.path('/restaurants');
-				
-			})
-			.error(function (data, status, headers, config) {
+		alertService.clear();
+		$http.post('http://izdoors/login/', $.param($scope.credentials), {headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'}}).then(
+			function successCallback (response) {
+				if(response.data.msg.code == 1){
+					$window.localStorage.token = response.data.token;
+					$rootScope.isAuthenticated = true;
+					$location.path('/opendoor');
+				}else{
+					console.log(response.data.msg.text);
+					delete $window.localStorage.token;
+					$rootScope.isAuthenticated = false;
+					alertService.add('danger', 'Invalid Login, please try again.');
+				}
+			},
+			function errorCallback (response) {
 				// Erase the token if the user fails to log in
+				console.log("login fail");
 				delete $window.sessionStorage.token;
-				$scope.auth_error = data;
+				$scope.auth_error = data.msg.text;
 			})
 	};
+}]);
+
+izcontrollers.controller('opendoorCtrl', ['$scope', '$rootScope', '$location', '$http', '$window', 'alertService', function($scope, $rootScope, $location, $http, $window, alertService) {
+	if (!$window.localStorage.token){
+		$location.path('/login');
+	}
+	$scope.logout = function(){
+		delete $window.localStorage.token;
+		$location.path('/login');
+		return;
+	}
+	
+	$rootScope.closeAlert = alertService.closeAlert;
+	
+	$scope.opendoor = function(door){
+		$http.get('http://10.8.25.11/door/' + door + '/').then(
+				function successCallback (response) {
+					if(response.data.msg.code === 2){
+						alertService.add('success', response.data.msg.text, 5000);
+					}else{
+						alertService.add('danger', response.data.msg.text);
+						$location.path('/login');
+					}
+				},
+				function errorCallback (response) {
+					$http.get('https://doors.impactzero.pt:2096/door/' + door + '/').then(
+						function success(resp){
+							if(response.data.msg.code === 2){
+								alertService.add('success', response.data.msg.text, 5000);
+							}else{
+								alertService.add('danger', response.data.msg.text);
+								$location.path('/login');
+							}
+						},
+						function error(resp){
+							if(response.data.msg.text){
+								alertService.add('danger', response.data.msg.text);
+							}
+							else{
+								$location.path('/login');
+							}
+							
+						}
+					);
+				});
+	}
 }]);
--- MyServer/public/app/izdoors.js
+++ MyServer/public/app/izdoors.js
@@ -1,12 +1,68 @@
-var izdoors = angular.module('izdoors', [ 'ngRoute', 'izcontrollers' ]).config(
-		function($routeProvider) {
-			$routeProvider.when('/', {
-				controller : 'homeCtrl',
-				templateUrl : '/app/views/izhome.html'
-			}).when('/login', {
-				controller : "loginCtrl",
-				templateUrl : "/app/views/izlogin.html"
-			}).otherwise({
-				redirectTo : '/'
-			});
+var izdoors = angular.module('izdoors', [ 'ngRoute', 'izcontrollers' ])
+		.config(function($routeProvider) {
+			$routeProvider
+				.when('/login', {
+					controller : 'loginCtrl',
+					templateUrl : '/app/views/izlogin.html'
+				})
+				.when('/opendoor', {
+					controller : 'opendoorCtrl',
+					templateUrl : '/app/views/izopendoor.html'
+				})
+				.otherwise({
+					redirectTo : '/login'
+				});
 		})
+		.config(function($httpProvider) {
+			$httpProvider.interceptors.push('authInterceptor');
+		});
+
+izdoors.factory('authInterceptor',function($rootScope, $q, $window) {
+	return {
+		request : function(config) {
+			config.headers = config.headers || {};
+			config.headers["X-IZDoors-API"] = '2';
+			if ($window.localStorage.token) {
+				config.headers["X-IZDoors-Token"] = $window.localStorage.token;
+			}
+			return config;
+		},
+		response : function(response) {
+			if (response.status === 401) {
+				// handle the case where the user is not
+				// authenticated
+			}
+			return response || $q.when(response);
+		}
+	};
+});
+
+izdoors.factory('alertService', function($rootScope, $timeout) {
+    var alertService = {};
+    $rootScope.alerts = [];
+    alertService.add = function(type, msg, timeout) {
+    	$rootScope.alerts.push({
+    		type: type,
+            msg: msg,
+            close: function() {
+                return alertService.closeAlert(this);
+            }
+        });
+
+        if (timeout) {
+            $timeout(function(){ 
+                alertService.closeAlert(this); 
+            }, timeout); 
+        }
+    };
+
+    alertService.closeAlert = function(index) {
+    	$rootScope.alerts.splice(index, 1);
+    };
+    
+    alertService.clear = function() {
+    	$rootScope.alerts = [];
+    }
+
+    return alertService;
+  });
