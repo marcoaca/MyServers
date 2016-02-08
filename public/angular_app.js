8798dcd715cdf05b2463c96dc1960ddaac0b6b33
marco amaral <amaral.marco@gmail.com>
2016-02-08 16:56:19 +0000

    mudanças

--- public/angular_app.js
+++ public/angular_app.js
@@ -15,3 +15,22 @@ myapp.config(['$routeProvider','$locationProvider',
 		$locationProvider.html5Mode(true);
 	}
 ]);
+
+myapp.factory('authInterceptor', function($rootScope, $q, $window) {
+	return {
+		request : function(config) {
+			config.headers = config.headers || {};
+			if ($window.sessionStorage.token) {
+				config.headers.Authorization = 'Token '
+					+ $window.sessionStorage.token;
+			}
+			return config;
+		},
+		response : function(response) {
+			if (response.status === 401) {
+				// handle the case where the user is not authenticated
+			}
+			return response || $q.when(response);
+		}
+	};
+});
